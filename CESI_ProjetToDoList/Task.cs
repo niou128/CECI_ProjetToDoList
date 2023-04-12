7094333146eaf60b0a63b15b2df86a3551c77be5
Sébastien Bernard <niou128@hotmail.com>
2023-04-12 13:51:30 +0200

    Utilisation de EFCore et LinQ

--- CESI_ProjetToDoList/Task.cs
+++ CESI_ProjetToDoList/Task.cs
@@ -1,7 +1,10 @@
-﻿namespace CESI_ProjetToDoList
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CESI_ProjetToDoList
 {
     public class Task
     {
+        [Key] // Attribut pour indiquer que Id est la clé primaire
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Date { get; set; }
--- CESI_ProjetToDoList/ToDoListContext.cs
+++ CESI_ProjetToDoList/ToDoListContext.cs
@@ -1,138 +1,70 @@
-﻿using System.Data.SQLite;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace CESI_ProjetToDoList
 {
-    public class ToDoListContext
+    public class ToDoListContext : DbContext
     {
-        private string connectionString = "Data Source=todo.db3";
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=todo.db3");
+        }
 
         public void InitializeDatabase()
         {
-            if (!File.Exists("todo.db3"))
+            using (var context = new ToDoListContext())
             {
-                SQLiteConnection.CreateFile("todo.db3");
-
-                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
-                {
-                    conn.Open();
-                    string query = @"CREATE TABLE Tasks (
-                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                Name TEXT NOT NULL,
-                                Date TEXT NOT NULL,
-                                IsCompleted INTEGER NOT NULL)";
-                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
-                    {
-                        cmd.ExecuteNonQuery();
-                    }
-                    conn.Close();
-                }
+                // Crée la base de données si elle n'existe pas
+                context.Database.EnsureCreated();
             }
         }
 
         public List<Task> GetAllTasks()
         {
-            List<Task> tasks = new List<Task>();
-
-            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            using (var context = new ToDoListContext())
             {
-                conn.Open();
-                string query = "SELECT * FROM Tasks";
-                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
-                {
-                    using (SQLiteDataReader reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            tasks.Add(new Task
-                            {
-                                Id = reader.GetInt32(0),
-                                Name = reader.GetString(1),
-                                Date = DateTime.Parse(reader.GetString(2)),
-                                IsCompleted = reader.GetBoolean(3)
-                            });
-                        }
-                    }
-                }
-                conn.Close();
+                var tasks = context.Tasks.ToList();
+                return tasks;
             }
-
-            return tasks;
         }
 
         public void AddTask(Task task)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            using (var context = new ToDoListContext())
             {
-                conn.Open();
-                string query = "INSERT INTO Tasks (Name, Date, IsCompleted) VALUES (@Name, @Date, @IsCompleted)";
-                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Name", task.Name);
-                    cmd.Parameters.AddWithValue("@Date", task.Date.ToString("yyyy-MM-dd HH:mm"));
-                    cmd.Parameters.AddWithValue("@IsCompleted", task.IsCompleted);
-                    cmd.ExecuteNonQuery();
-                }
-                conn.Close();
+                context.Tasks.Add(task);
+                context.SaveChanges();
             }
         }
 
         public void UpdateTask(Task task)
         {
-            using (var conn = new SQLiteConnection(connectionString))
+            using (var context = new ToDoListContext())
             {
-                conn.Open();
-                string query = "UPDATE Tasks SET Name = @Name, Date = @Date, IsCompleted = @IsCompleted WHERE Id = @Id";
-                using (var cmd = new SQLiteCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", task.Id);
-                    cmd.Parameters.AddWithValue("@Name", task.Name);
-                    cmd.Parameters.AddWithValue("@Date", task.Date);
-                    cmd.Parameters.AddWithValue("@IsCompleted", task.IsCompleted);
-                    cmd.ExecuteNonQuery();
-                }
+                context.Tasks.Update(task);
+                context.SaveChanges();
             }
         }
 
         public Task GetTaskById(int id)
         {
-            using (var conn = new SQLiteConnection(connectionString))
+            using (var context = new ToDoListContext())
             {
-                conn.Open();
-                string query = "SELECT * FROM Tasks WHERE Id = @Id";
-                using (var cmd = new SQLiteCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", id);
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return new Task
-                            {
-                                Id = reader.GetInt32(0),
-                                Name = reader.GetString(1),
-                                Date = reader.GetDateTime(2),
-                                IsCompleted = reader.GetBoolean(3)
-                            };
-                        }
-                    }
-                }
+                return context.Tasks.FirstOrDefault(task => task.Id == id);
             }
-
-            return null;
         }
 
         public void DeleteTask(int id)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            using (var context = new ToDoListContext())
             {
-                conn.Open();
-                string query = "DELETE FROM Tasks WHERE Id = @Id";
-                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                var task = context.Tasks.FirstOrDefault(t => t.Id == id);
+                if (task != null)
                 {
-                    cmd.Parameters.AddWithValue("@Id", id);
-                    cmd.ExecuteNonQuery();
+                    context.Tasks.Remove(task);
+                    context.SaveChanges();
                 }
-                conn.Close();
             }
         }
 
